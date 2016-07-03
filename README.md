040e7a43e6536ed304f873ff7be065c5a5cb5c02
Peter Ogilvie <code@ogilvie.us.com>
2016-07-02 18:13:49 -0700

    login

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Create a Salesforce Custom Object with fields and visible FLS
 cred.js
 -------
 
-Add the file cred.js with your Salesforce Credential to the project
+Add the file 'cred.js' with your Salesforce Credential to the project
 root director of the form
 
-    exports.cred = { username : 'user@domain.org' , password : 'yourpassword'}
+    exports.cred = { username : 'user@domain.org' , password : 'yourpassword', token: 'SecurityToken'}
--- meta.js
+++ meta.js
@@ -1,6 +1,19 @@
 'use strict'
 
 const jsforce = require('jsforce'),
-         cred = require('./cred.js').cred
+         cred = require('./cred.js').cred,
+            c = new jsforce.Connection({loginUrl : 'https://test.salesforce.com'})
 
 console.log(cred)
+
+c.login(cred.username, cred.password + cred.token, function(err, userInfo) {
+
+    if (err)
+        throw err
+
+    console.log('Access Token:' + c.accessToken)
+    console.log('Instance URL:' + c.instanceUrl)
+    console.log("User ID: " + userInfo.id)
+    console.log("Org ID: " + userInfo.organizationId)
+
+})
